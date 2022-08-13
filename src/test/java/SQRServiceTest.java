1397a1abfa8baaa787799e299022a52fd929fb93
Vadim Turanosoff <turanosoff@gmail.com>
2022-08-13 21:01:36 +0300

    initial commit

---
+++ src/test/java/SQRServiceTest.java
@@ -0,0 +1,30 @@
+package ru.netology.sqr;
+
+import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.Test;
+
+public class SQRServiceTest {
+
+    @Test
+    public void shouldCount() {
+        SQRService service = new SQRService() {
+            int count = service.countSqr(200, 300)
+            int expected = 3;
+                    Assertions.assertEquals(expected,actual);
+            @Test
+            public void shouldCount() {
+                SQRService service = new SQRService() {
+                    int count = service.countSqr(300, 200)
+                    int expected = 3;
+                    Assertions.assertEquals(expected,actual);
+                    @Test
+                    public void shouldCount() {
+                        SQRService service = new SQRService() {
+                            int count = service.countSqr(200, 200)
+                            int expected = 3;
+                    Assertions.assertEquals(expected,actual);
+
+        }
+    }
+
